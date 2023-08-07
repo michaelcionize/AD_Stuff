4b7b9724e8499f4b4ed4047ae3317fb22afa3167
michaelcionize <97269630+michaelcionize@users.noreply.github.com>
2023-08-07 13:36:31 +1000

    Update AD_Objects.ps1

--- AD_Objects.ps1
+++ AD_Objects.ps1
@@ -1,14 +1,20 @@
+# Check if the Active Directory module is loaded
+if (-not (Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue)) {
+    Write-Host "Active Directory module not found. Please install the module first."
+    exit 1
+}
+
 # Get the current user's distinguished name (DN)
 $currentUserDN = (Get-ADUser -Identity $env:USERNAME).DistinguishedName
 
 # Get all AD objects where the current user has explicit permissions
-$permissionsObjects = Get-ADPermission -Identity $currentUserDN -User $currentUserDN -ResultPageSize 1000 -AuthType Kerberos -ErrorAction SilentlyContinue |
-    Select-Object Identity, ObjectType, InheritedObjectType |
-    Sort-Object Identity
+$adObjects = Get-ADObject -Filter * -Properties nTSecurityDescriptor |
+             Where-Object {($_.nTSecurityDescriptor.Access | Where-Object {$_.IdentityReference -eq $currentUserDN}) -ne $null} |
+             Select-Object DistinguishedName, ObjectClass
 
 # Output the results
-if ($permissionsObjects) {
-    $permissionsObjects | Format-Table -AutoSize
+if ($adObjects) {
+    $adObjects | Format-Table -AutoSize
 } else {
     Write-Host "No objects found where the current user has explicit permissions."
 }
